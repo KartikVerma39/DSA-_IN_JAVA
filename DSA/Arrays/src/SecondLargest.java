36e1bd3c0c82112463e25624aa37974f730ea878
KartikVerma39 <kartikverma1074@gmail.com>
2026-02-04 11:44:37 +0530

    DSA_IN_JAVA

---
+++ DSA/Arrays/src/SecondLargest.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // code here
+        int largest = arr[0];
+        int secondLargest = -1;
+
+        for(int i = 1 ; i < arr.length ; i++){
+            if(arr[i] > largest){
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            if(arr[i] < largest && arr[i] > secondLargest){
+                secondLargest = arr[i];
+            }
+        }
+
+        return secondLargest;
+
+    }
+}
