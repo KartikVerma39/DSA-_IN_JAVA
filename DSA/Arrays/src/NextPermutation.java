445371ba426605212905592f59c84905ac0b6e90
KartikVerma39 <kartikverma1074@gmail.com>
2026-02-12 15:18:15 +0530

    DSA_IN_JAVA

--- DSA/Arrays/src/NextPermutation.java
+++ DSA/Arrays/src/NextPermutation.java
@@ -41,7 +41,7 @@ public class NextPermutation {
         }
 
         for(int i = 0 ; i < n ; i++){
-            System.out.print(arr[i] + " ");
+            System.out.print(arr[i] + "  ");
         }
     }
 }
